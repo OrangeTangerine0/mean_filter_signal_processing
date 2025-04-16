e7ad8dc27b7d88fbdb126a72584b9c4fab095de0
OrangeTangerine0 <arjungitabg@gmail.com>
2025-04-15 21:03:06 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+
+echo "Compiling..."
+make
+
+echo "Creating output directory..."
+mkdir -p output
+
+echo "Running mean filter signal processing..."
+./signal_proc > log.txt
+
+echo "Done. Output saved in output/ and log.txt"
